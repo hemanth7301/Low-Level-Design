652f71bf23b5e49df34ec82775a1151b2f50059d
hemanth7301 <saihemanthsakhamuri@gmail.com>
2024-03-14 23:07:41 +0530

    Snake and Ladder

---
+++ SnackAndLadder/Player.java
@@ -0,0 +1,8 @@
+public class Player{
+    String id;
+    int playerPosition;
+    public Player(String id,int playerPosition){
+        this.id=id;
+        this.playerPosition=playerPosition;
+    }
+}
